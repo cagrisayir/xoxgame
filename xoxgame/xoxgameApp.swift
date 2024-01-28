5b4fae8015454dcb04c748bfa97cbf19a59bb0ed
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-28 10:38:50 +0300

    Initial Commit

---
+++ xoxgame/xoxgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  xoxgameApp.swift
+//  xoxgame
+//
+//  Created by Omer Cagri Sayir on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct xoxgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
